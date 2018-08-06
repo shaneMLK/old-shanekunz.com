eb0a84781221d8a1b2cbd8162cd6a01d129c1f7f
Shane Michael Kunz <shanemkunz@gmail.com>
2018-08-06 16:06:16 -0500

    add serving of index.html

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
-
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 //app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
