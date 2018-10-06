750257e67ccdb6e105e36e0766ff700701e96bd5
Shane Michael Kunz <shanemkunz@gmail.com>
2018-10-06 14:16:31 -0500

    simplify things

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const messages = require('./db/messages');
 
-//var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-app.use(morgan('tiny'));
 app.use(cors());
 app.use(bodyParser.json());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(logger('tiny'));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
@@ -38,23 +25,17 @@ app.get('/messages', (req, res) => {
   });
 });
 
-//app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.post('/messages', (req, res) => {
+  console.log(req.body);
+  messages.create(req.body).then((message) => {
+      res.json(message);
+  }).catch((error) => {
+      res.status(500);
+      res.json(error);
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
 });
-
-module.exports = app;
